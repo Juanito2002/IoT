5ea251d1543259fea176400ec0267fc2cab9b7a6
Juanito <jetiraca.8720@unicesmag.edu.co>
2023-09-08 12:06:49 -0500

    Led hello world Arduine

---
+++ Arduino/LED13/LED13.ino
@@ -0,0 +1,14 @@
+//JUANITO IGUA
+int led=13;
+void setup() {
+  // put your setup code here, to run once:
+ pinMode(led,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(led,HIGH);
+  delay(1000);//1000=1seg
+  digitalWrite(led,LOW);
+  delay(1000);
+}
